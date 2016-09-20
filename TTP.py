79e2fc7315d44b4b292969913aed29f3108e5a4c
Yiying Wang <wang482@email.sc.edu>
2016-09-20 10:08:15 -0400

    feat: implement post-Order Traversal

--- TTP.py
+++ TTP.py
@@ -1,5 +1,5 @@
 '''
-Coding up for tree traversal practice quiz. Traverse the tree in four different orders.
+Coding up for binary tree traversal practice quiz. Traverse the tree in four different orders.
 
 		A
       /   \
@@ -20,9 +20,38 @@ Post-order: D, F, E, B, C, A
 class Node:
 	def __init__(self, value):
 		self.value = value
-		self.children = None
+		self.left = None
+		self.right = None
 
-class Tree:
-	def __init__(self, root=None):
-		self.root = root
+
+def postOrderTraversal(node, output):
+	if node != None:
+		postOrderTraversal(node.left, output)
+		postOrderTraversal(node.right, output)
+		output.append(node.value)
+	return output
+
+
+
+
+A = Node('A')
+B = Node('B')
+C = Node('C')
+D = Node('D')
+E = Node('E')
+F = Node('F')
+
+A.left = B
+A.right = C 
+B.left = D 
+B.right = E 
+E.right = F 
+
+output = []
+output = postOrderTraversal(A, output)
+print output
+
+
+
+
 
