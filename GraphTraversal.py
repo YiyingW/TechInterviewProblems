14329252f6eb9257377948db8bd243f5f2a2b520
Yiying Wang <wang482@email.sc.edu>
2016-09-22 14:56:16 -0400

    feat: create a branch solution. Copy solution for GraphTraversal. It is buggy.

--- GraphTraversal.py
+++ GraphTraversal.py
@@ -131,25 +131,15 @@ class Graph(object):
         for node in self.nodes:
             node.visited = False
 
-    def dfs_helper(self, start_node, ret_list=None):
-        """TODO: Write the helper function for a recursive implementation
-        of Depth First Search iterating through a node's edges. The
-        output should be a list of numbers corresponding to the
-        values of the traversed nodes.
-        ARGUMENTS: start_node is the starting Node
-        MODIFIES: the value of the visited property of nodes in self.nodes 
-        RETURN: a list of the traversed node values (integers).
-        """
-        if ret_list == None: 
-        	ret_list = [start_node.value]
-        else:
-        	ret_list.append(start_node.value)
-        # Your code here
-        start_node.visited = True
-        for edge in start_node.edges:
-        	if edge.node_to.visited == False:
-        		self.dfs_helper(edge.node_to, ret_list)
-        return ret_list
+	def dfs_helper(self, start_node):
+		ret_list = [start_node.value]
+    	start_node.visited = True
+    	edges_out = [e for e in start_node.edges if e.node_to.value != start_node.value]
+    	for edge in edges_out:
+    		if not edge.node_to.visited:
+    			ret_list.extend(self.dfs_helper(edge.node_to))
+    	return ret_list
+
 
 
     def dfs(self, start_node_num):
@@ -166,30 +156,27 @@ class Graph(object):
         """Return the results of dfs with numbers converted to names."""
         return [self.node_names[num] for num in self.dfs(start_node_num)]
 
-    def bfs(self, start_node_num):
-        """TODO: Create an iterative implementation of Breadth First Search
-        iterating through a node's edges. The output should be a list of
-        numbers corresponding to the traversed nodes.
-        ARGUMENTS: start_node_num is the node number (integer)
-        MODIFIES: the value of the visited property of nodes in self.nodes
-        RETURN: a list of the node values (integers)."""
-        node = self.find_node(start_node_num)
-        self._clear_visited()
-        ret_list = []
+	def bfs(self, start_node_num):
+
+		node = self.find_node(start_node_num)
+		self._clear_visited()
+		ret_list = []
         # Your code here
-        queue = []
-        node.visited = True
-        queue.append(node)
-
-        while (len(queue) != 0):
-        	node = queue.pop(0)
-        	ret_list.append(node.value)
-        	for edge in node.edges:
-        		if (edge.node_to.visited == False):
-        			edge.node_to.visited = True
-        			queue.append(edge.node_to)
-
-        return ret_list
+		queue = [node]
+		node.visited = True
+		def enqueue(n, q=queue):
+			n.visited = True
+			q.append(n)
+		def unvisited_outgoing_edge(n, e):
+			return ((e.node_from.value == n.value) and
+					(not e.node_to.visited))
+		while queue:
+			node = queue.pop(0)
+			ret_list.append(node.value)
+			for e in node.edges:
+				if unvisited_outgoing_edge(node, e):
+					enqueue(e.node_to)
+		return ret_list
 
     def bfs_names(self, start_node_num):
         """Return the results of bfs with numbers converted to names."""
@@ -240,8 +227,8 @@ pp.pprint(graph.get_adjacency_list_names())
 print "\nAdjacency Matrix"
 pp.pprint(graph.get_adjacency_matrix())
 
-print "\nDepth First Search"
-pp.pprint(graph.dfs_names(2))
+# print "\nDepth First Search"
+# pp.pprint(graph.dfs_names(2))
 
 # Should print:
 # Depth First Search
