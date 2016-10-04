fe0f45fdebcc0f58140a6d516daf8776ca894961
Yiying Wang <wang482@email.sc.edu>
2016-10-03 20:18:30 -0400

    feat: add code to calculate the number of operations for sort

--- hackerrank/insertionsort2.py
+++ hackerrank/insertionsort2.py
@@ -4,27 +4,33 @@ https://www.hackerrank.com/challenges/insertionsort2
 utilize the insertionsort 1 to create the complete insertion sort function
 '''
 
-def subInsertionSort(arr, size):
+def subInsertionSort(arr, size, shift):
 	e = arr[size-1]
 	for i in range(size-1, -1, -1):
-		this = arr[i-1]
-		if this > e and i != 0:
+		this = arr[i-1]   
+		if this > e and i != 0: # the smaller one is at back, shift
 			arr[i] = this
-		elif this > e and i == 0:
+			shift += 1
+		elif this > e and i == 0: 
 			arr[i] = this
 			arr[0] = e
-			return arr
+			return (arr, shift)
 		else:
-			arr[i] = e
-			return arr
+			arr[i] = e  # no shift
+			return (arr, shift)
 			break
 
 s = int(raw_input())
 ar = map(int, raw_input().strip().split())
 
+shift = 0
 for i in range(2, s+1):
-	ar[:i] = subInsertionSort(ar[:i], i)
-	print ' '.join(map(str, ar))
+	(ar[:i], shift) = subInsertionSort(ar[:i], i, shift)
+
+
+	#print ' '.join(map(str, ar))
+print shift 
+
 	
 
 
