b8eaa959e4ed90ee6599477e82b3a5c50ef33b5f
Yiying Wang <wang482@email.sc.edu>
2016-12-12 17:05:56 -0500

    feat: solution

--- hackerrank/leetcode15.py
+++ hackerrank/leetcode15.py
@@ -6,7 +6,7 @@ Find all unique triplets in the array which gives the sum of zero.
 '''
 from collections import defaultdict
 
-def threeSum(nums):
+def threeSum1(nums):
     num_dict={}
     value_index_dict = defaultdict(list)
     for i, num in enumerate(nums):
@@ -37,5 +37,59 @@ def threeSum(nums):
     return output
 
 S = [-1, 0, 1, 2, -1, -4]
-print threeSum(S)
+#print threeSum1(S)
+
+class Solution(object):
+
+    def threeSum(self, nums):
+        nums.sort() # sort the list in place
+        res = []
+        length = len(nums)
+        for i in range(0, length-2):
+            if i and nums[i] == nums[i-1]: # because you don't want duplicate
+                continue # not do anything, next for loop option
+            target = nums[i] * -1
+            left, right = i + 1, length - 1
+            while left < right:
+                if nums[left] + nums[right] == target:
+                    res.append([nums[i], nums[left], nums[right]])
+                    right -= 1
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                else:
+                    left += 1
+
+        return res 
+
+
+
+
+ans = Solution()
+print ans.threeSum(S)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
