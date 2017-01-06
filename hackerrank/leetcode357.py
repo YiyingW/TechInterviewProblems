39c37cea6216d7dddacb59ffd591edbf8620c519
Yiying Wang <wang482@email.sc.edu>
2017-01-05 19:15:32 -0500

    feat: AC solution

--- hackerrank/leetcode357.py
+++ hackerrank/leetcode357.py
@@ -1,53 +1,39 @@
 class Solution(object):
     def countNumbersWithUniqueDigits(self, n):
-        def subCountNumbers(n):
-            """
-            :type n: int
-            :rtype: int
-            """
-            if n == 0: return 1
-            result = []
-            def adddigit(output, num, n):
-                if num == n:
-                    result.append(output)
-                else:
-                    if len(output)>=1:
-                        if output[0] != "0" and "0" not in output:
-                            adddigit(output+"0", num+1, n)
-                    if "1" not in output:
-                        adddigit(output+"1", num+1, n)                    
-                    if "2" not in output:
-                        adddigit(output+"2", num+1, n)
-                    if "3" not in output:
-                        adddigit(output+"3", num+1, n)
-                    if "4" not in output:
-                        adddigit(output+"4", num+1, n)
-                    if "5" not in output:
-                        adddigit(output+"5", num+1, n)
-                    if "6" not in output:
-                        adddigit(output+"6", num+1, n)
-                    if "7" not in output:
-                        adddigit(output+"7", num+1, n)  
-                    if "8" not in output:
-                        adddigit(output+"8", num+1, n)
-                    if "9" not in output:
-                        adddigit(output+"9", num+1, n) 
-
-
-            adddigit("", 0, n)
-            return len(result)
-
-        
-        if n == 0: return subCountNumbers(n)
-        else:
-            final_result = subCountNumbers(n)
+        def cal_combination(n):
+            result = 1
             while (n>=1):
-                final_result += subCountNumbers(n-1)
+                result *= n
                 n -= 1
-        return final_result
+            return result
+        
+        def uniqueDigit(n): # output n digits cases only
+            num = 0
+            subresult = []
+            def search(start, current, cnt):
+                if cnt == n:
+                    subresult.append(current)
+                    return
+                for i in range(start, 10):
+                    search(i+1, current+str(i), cnt+1)  
+            search(0, "", 0)
+            print subresult
+            
+            for case in subresult:
+                if '0' in case:
+                    num += cal_combination(n) - cal_combination(n-1)
+                else:
+                    num += cal_combination(n)
+            
+            return num
 
+        result = 0
+        for i in range(0, n+1):
+            result += uniqueDigit(i)
+            print result
+        return result
 
 c = Solution()
-print c.countNumbersWithUniqueDigits(8)        
+print c.countNumbersWithUniqueDigits(4)        
 
 
