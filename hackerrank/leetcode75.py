7565eca7e989def681563c61e8b441d9b1ad5002
Yiying Wang <YiyingW@users.noreply.github.com>
2017-02-09 10:59:14 -0500

    sort colors

---
+++ hackerrank/leetcode75.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        
+        i, j = 0, 0
+        for k in range(n):
+            v = nums[k]
+            nums[k] = 2
+            if v < 2:
+                nums[j] = 1
+                j += 1
+            if v == 0:
+                nums[i] = 0
+                i += 1
+        
